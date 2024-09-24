aaf215e805ce5072a7e6d7e2f749d01a7c9b346f
alisson <alison_26122@hotmail.com>
2024-09-23 19:03:01 -0500

    se ajusta controlador y modelo de carrier

--- app/Http/Controllers/CarriersController.php
+++ app/Http/Controllers/CarriersController.php
@@ -8,14 +8,17 @@ use Illuminate\Validation\ValidationException;
 
 class CarriersController extends Controller
 {
+    protected $carriersModel;
+    public function __construct()
+    {
+        $this->carriersModel = new CarriersModel();
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        $carriers = CarriersModel::where('transportadora_estatus', 1)->get();
-        $carriersArray = $carriers->toArray();
-        return response()->json($carriersArray, 200);
+        return $this->carriersModel->getCarriers();
     }
 
     /**
@@ -23,50 +26,40 @@ class CarriersController extends Controller
      */
     public function store(Request $request)
     {
-        try{
+        try {
             $validatedData = $request->validate([
-                'transportadora_razon_social'=>'required|string',
-                'transportadora_nit'=>'required|string',
-                'transportadora_direccion'=>'required|string',
-                'transportadora_telefono'=>'required|string',
-                'transportadora_dv'=>'required|numeric',
-                'transportadora_ciudad'=>'required|string',
-                'transportadora_departamento'=>'required|string',
-                'transportadora_estatus'=>'required|numeric',
+                'transportadora_razon_social' => 'required|string',
+                'transportadora_nit' => 'required|string',
+                'transportadora_direccion' => 'required|string',
+                'transportadora_telefono' => 'required|string',
+                'transportadora_dv' => 'required|numeric',
+                'transportadora_ciudad' => 'required|string',
+                'transportadora_departamento' => 'required|string',
+                'transportadora_estatus' => 'required|numeric',
             ]);
-            $carrier = new CarriersModel();
-            $carrier->transportadora_razon_social = $validatedData['transportadora_razon_social'];
-            $carrier->transportadora_nit = $validatedData['transportadora_nit']; 
-            $carrier->transportadora_direccion = $validatedData['transportadora_direccion'];
-            $carrier->transportadora_telefono = $validatedData['transportadora_telefono'];
-            $carrier->transportadora_dv = $validatedData['transportadora_dv'];
-            $carrier->transportadora_ciudad = $validatedData['transportadora_ciudad'];
-            $carrier->transportadora_departamento = $validatedData['transportadora_departamento'];
-            $carrier->transportadora_estatus = $validatedData['transportadora_estatus'];
-            $carrier->save();
-        // Redirigir o retornar una respuesta
-        return response()->json([
-            'estatus_guardado' => 1,
-            'mensaje' => 'Datos guardados correctamente. :)'
-        ]);
-
-    } catch (ValidationException $e) {
-        return response()->json([
-            'estatus_guardado' => 0,
-            'mensaje' => 'uno o más datos son incorrectos',
-            'error' => $e->getMessage()
-        ], 422);
-    }
+            $carrier = $this->carriersModel->saveCarrier($validatedData);
+            // Redirigir o retornar una respuesta
+            return response()->json([
+                'estatus_guardado' => 1,
+                'mensaje' => 'Datos guardados correctamente. :)'
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'estatus_guardado' => 0,
+                'mensaje' => 'uno o más datos son incorrectos',
+                'error' => $e->getMessage()
+            ], 422);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show( $carrier_id)
+    public function show($carrier_id)
     {
-        $route =  CarriersModel::find($carrier_id);
-        if (!empty($route)) {
-            return response()->json($route, 200);
+        $carrier = $this->carriersModel->getCarrier($carrier_id);
+        if (!empty($carrier)) {
+            return response()->json($carrier, 200);
         } else {
             return response()->json([
                 'message' => 'registro no encontrado'
@@ -80,53 +73,51 @@ class CarriersController extends Controller
      */
     public function update(Request $request, string $carrier_id)
     {
-        try{
+        try {
+            // Validar datos
             $validatedData = $request->validate([
-                'transportadora_razon_social'=>'required|string',
-                'transportadora_nit'=>'required|string',
-                'transportadora_direccion'=>'required|string',
-                'transportadora_telefono'=>'required|string',
-                'transportadora_dv'=>'required|numeric',
-                'transportadora_ciudad'=>'required|string',
-                'transportadora_departamento'=>'required|string',
-                'transportadora_estatus'=>'required|numeric',
+                'transportadora_razon_social' => 'required|string',
+                'transportadora_nit' => 'required|string',
+                'transportadora_direccion' => 'required|string',
+                'transportadora_telefono' => 'required|string',
+                'transportadora_dv' => 'required|numeric',
+                'transportadora_ciudad' => 'required|string',
+                'transportadora_departamento' => 'required|string',
+                'transportadora_estatus' => 'required|numeric',
             ]);
-            $carrier = CarriersModel::find($carrier_id);
-            $carrier->transportadora_razon_social = $validatedData['transportadora_razon_social'];
-            $carrier->transportadora_nit = $validatedData['transportadora_nit']; 
-            $carrier->transportadora_direccion = $validatedData['transportadora_direccion'];
-            $carrier->transportadora_telefono = $validatedData['transportadora_telefono'];
-            $carrier->transportadora_dv = $validatedData['transportadora_dv'];
-            $carrier->transportadora_ciudad = $validatedData['transportadora_ciudad'];
-            $carrier->transportadora_departamento = $validatedData['transportadora_departamento'];
-            $carrier->transportadora_estatus = $validatedData['transportadora_estatus'];
-            $carrier->save();
-        // Redirigir o retornar una respuesta
-        return response()->json([
-            'estatus_guardado' => 1,
-            'mensaje' => 'Datos actualizados correctamente. :)'
-        ]);
-
-    } catch (ValidationException $e) {
-        return response()->json([
-            'estatus_guardado' => 0,
-            'mensaje' => 'uno o más datos son incorrectos',
-            'error' => $e->getMessage()
-        ], 422);
+    
+            // Encontrar la transportadora
+            $carrier = $this->carriersModel->getCarrier($carrier_id);
+    
+            // Actualizar la transportadora usando el modelo
+            $carrier->updateCarrier($validatedData);
+    
+            // Retornar respuesta exitosa
+            return response()->json([
+                'estatus_guardado' => 1,
+                'mensaje' => 'Datos actualizados correctamente. :)'
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'estatus_guardado' => 0,
+                'mensaje' => 'Uno o más datos son incorrectos.',
+                'error' => $e->getMessage()
+            ], 422);
+        }
     }
-    }
+    
 
     /**
      * Remove the specified resource from storage.
      */
     public function delete($carrier_id)
     {
-        $carrier = CarriersModel::find($carrier_id);
+        $carrier = $this->carriersModel->getCarrier($carrier_id);
         if ($carrier) {
             // Actualizar el campo estado_eliminar a 0
             $carrier->transportadora_estatus = 0;
             $carrier->save();
-            
+
             return response()->json([
                 "message" => "Registro eliminado con exito"
             ]);
--- app/Models/CarriersModel.php
+++ app/Models/CarriersModel.php
@@ -21,4 +21,41 @@ class CarriersModel extends Model
         'transportadora_departamento',
         'transportadora_estatus',
     ];
+
+    public function getCarriers()
+    {
+        $carriers = CarriersModel::where('transportadora_estatus', 1)->get();
+        $carriersArray = $carriers->toArray();
+        return response()->json($carriersArray, 200);
+    }
+    public function saveCarrier($validatedData)
+    {
+        $this->transportadora_razon_social = $validatedData['transportadora_razon_social'];
+        $this->transportadora_nit = $validatedData['transportadora_nit'];
+        $this->transportadora_direccion = $validatedData['transportadora_direccion'];
+        $this->transportadora_telefono = $validatedData['transportadora_telefono'];
+        $this->transportadora_dv = $validatedData['transportadora_dv'];
+        $this->transportadora_ciudad = $validatedData['transportadora_ciudad'];
+        $this->transportadora_departamento = $validatedData['transportadora_departamento'];
+        $this->transportadora_estatus = $validatedData['transportadora_estatus'];
+        $this->save();
+    }
+
+    public function getCarrier($carrier_id)
+    {
+        $carrier = $this->find($carrier_id);
+        return $carrier;
+    }
+    public function updateCarrier($validatedData)
+    {
+        $this->transportadora_razon_social = $validatedData['transportadora_razon_social'];
+        $this->transportadora_nit = $validatedData['transportadora_nit'];
+        $this->transportadora_direccion = $validatedData['transportadora_direccion'];
+        $this->transportadora_telefono = $validatedData['transportadora_telefono'];
+        $this->transportadora_dv = $validatedData['transportadora_dv'];
+        $this->transportadora_ciudad = $validatedData['transportadora_ciudad'];
+        $this->transportadora_departamento = $validatedData['transportadora_departamento'];
+        $this->transportadora_estatus = $validatedData['transportadora_estatus'];
+        $this->save();
+    }
 }
